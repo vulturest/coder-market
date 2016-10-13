2eba47ddc7a5a58fe2f36793265a2818ad20834d
chuxiuhong <nanwugongzi@vip.qq.com>
2016-10-13 16:28:39 +0800

    更新了数据库的需求并且已经完成了数据库的初始化。

--- coder_market/coder_market/settings.py
+++ coder_market/coder_market/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'coder_market.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'coder_market',
+        'USER':'root',
+        'PASSWORD':'',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
--- coder_market/coder_market_web/models.py
+++ coder_market/coder_market_web/models.py
@@ -2,4 +2,41 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+class user_login(models.Model):
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    identity = models.CharField(max_length=15)
+
+
+class publisher(models.Model):
+    username = models.CharField(max_length=30)
+    order_project = models.IntegerField()
+    presonal_information = models.CharField(max_length=100)
+    accept_information = models.TextField()
+
+
+class receiver(models.Model):
+    username = models.CharField(max_length=30)
+    get_project = models.IntegerField()
+    presonal_information = models.CharField(max_length=100)
+    tag = models.CharField(max_length=50)
+    accept_information = models.TextField()
+
+
+class manager(models.Model):
+    username = models.CharField(max_length=30)
+    manage_project = models.IntegerField()
+    presonal_information = models.CharField(max_length=100)
+    tag = models.CharField(max_length=50)
+    accept_information = models.TextField()
+
+
+class project(models.Model):
+    number = models.IntegerField()
+    project_publisher = models.CharField(max_length=30)
+    project_content = models.TextField()
+    project_receiver = models.CharField(max_length=30)
+    project_manager = models.CharField(max_length=30)
+    tag = models.CharField(max_length=50)
